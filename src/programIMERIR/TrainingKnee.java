8a48b05c464a6f626595a9a5f296328fb4de5e06
Pirard <Pirard@KUKA-PIRARD.villebon.kuka.fr>
2018-03-15 10:07:21 +0100

    --- automatic rebase to remote; old local SHA: 66f0d21fbdf18321c51805f38972f3e67e8af88b ---

--- src/programIMERIR/TrainingKnee.java
+++ src/programIMERIR/TrainingKnee.java
@@ -10,7 +10,7 @@ import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.geometricModel.Tool;
 import com.kuka.roboticsAPI.geometricModel.Workpiece;
-import com.kuka.roboticsAPI.persistenceModel.processDataModel.IProcessData;
+import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
 
 /**
  * Implementation of a robot application.
@@ -39,19 +39,24 @@ public class TrainingKnee extends RoboticsAPIApplication {
 	@Inject
 	@Named("Leg")
 	private Workpiece leg;
-	private int i;	
-	//var accessible via le processdata
-
-	private Integer   tempo, nbcycles;
-	private Double angle, anglespeed;
+	private int i;
+	// Variables accessibles via le ProcessData"
+	private Integer tempo,
+					nbCycles;
+	private Double angle,
+				   angleSpeed; 
+	//Création d'un entier qui récupère le choix de l'utilisateur
+	private int answer;
 	@Override
 	public void initialize() {
 		// initialize your application here
 		legLift.attachTo(robot.getFlange());
-		tempo = getApplicationData().getProcessData("tempo").getValue();
-		nbcycles = getApplicationData().getProcessData("nbcycles").getValue();
-		angle = getApplicationData().getProcessData("angle").getValue();
-		anglespeed = getApplicationData().getProcessData("anglespeed").getValue();
+		tempo=getApplicationData().getProcessData("tempo").getValue();
+		nbCycles=getApplicationData().getProcessData("nbCycles").getValue();
+		angle=getApplicationData().getProcessData("angle").getValue();
+		angleSpeed=getApplicationData().getProcessData("angleSpeed").getValue();
+		//initialisation de answer à une valeur non utilisée par la boîte de dialogue
+		answer=-1;
 	}
 
 	@Override
@@ -59,15 +64,20 @@ public class TrainingKnee extends RoboticsAPIApplication {
 		// your application execution starts here
 		robot.move(ptpHome());
 		legLift.getFrame("/dummy/pnpParent").move(ptp(getApplicationData().getFrame("/Knee/P1")).setJointVelocityRel(0.5));
+		// initialisation de la variable answer avec le message
+		answer=getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, "La jambe est elle en place?", "Oui");
 		ThreadUtil.milliSleep(tempo);
 		// Ancrage de la jambe à l'outil
 		leg.getFrame("/PnpChild").attachTo(legLift.getFrame("/dummy/pnpParent"));
-		for (i=1;i<nbcycles;i++){
-			leg.getFrame("TCPKnee").move(linRel(0, 0, 0, Math.toRadians(-angle),0, 0).setCartVelocity(anglespeed));
-			leg.getFrame("TCPKnee").move(linRel(0, 0, 0, Math.toRadians(angle),0, 0).setCartVelocity(anglespeed));
+		for (i=1;i<nbCycles;i++){
+			leg.getFrame("TCPKnee").move(linRel(0, 0, 0, Math.toRadians(-angle),0, 0).setCartVelocity(angleSpeed));
+			leg.getFrame("TCPKnee").move(linRel(0, 0, 0, Math.toRadians(angle),0, 0).setCartVelocity(angleSpeed));
 		}
 		ThreadUtil.milliSleep(tempo);
 		leg.detach();
 		robot.move(ptpHome().setJointVelocityRel(0.5));
+		
+		
+		
 	}
 }
