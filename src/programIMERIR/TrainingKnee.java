dc8ad04f2f0e1e1151b1d608bad1f9f6612e1c74
yannickFournier <yannick.fournier@imerir.com>
2018-03-15 15:34:39 +0100

    --- automatic rebase to remote; old local SHA: 0de20c0c13e38e3c74c490cea01fb5c4db95827d ---

--- src/programIMERIR/TrainingKnee.java
+++ src/programIMERIR/TrainingKnee.java
@@ -8,8 +8,10 @@ import com.kuka.common.ThreadUtil;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.CartDOF;
 import com.kuka.roboticsAPI.geometricModel.Tool;
 import com.kuka.roboticsAPI.geometricModel.Workpiece;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
 import com.kuka.roboticsAPI.persistenceModel.processDataModel.IProcessData;
 import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
 
@@ -49,6 +51,8 @@ public class TrainingKnee extends RoboticsAPIApplication {
 	//create d'un int qui recupere le choix de l'utilisateur
 	private int answer;
 	
+	private CartesianImpedanceControlMode ctrlMode;
+	
 	@Override
 	public void initialize() {
 		// initialize your application here
@@ -59,34 +63,36 @@ public class TrainingKnee extends RoboticsAPIApplication {
 		anglespeed = getApplicationData().getProcessData("anglespeed").getValue();
 		nom = getApplicationData().getProcessData("Nom").getValue();
 		answer = -1;
+		ctrlMode = new CartesianImpedanceControlMode();
+		ctrlMode.parametrize(CartDOF.X, CartDOF.Z).setStiffness(100);
+		ctrlMode.parametrize(CartDOF.Y).setStiffness(3000);
+		ctrlMode.parametrize(CartDOF.ALL).setDamping(0.7);
 	}
 
 	@Override
 	public void run() {
 		// your application execution starts here
 		robot.move(ptpHome());
-		legLift.getFrame("/dummy/pnpParent").move(ptp(getApplicationData().getFrame("/Knee/P1")).setJointVelocityRel(0.5));
+		legLift.getFrame("/dummy/pnpParent").move(ptp(getApplicationData().getFrame("/Knee/P1")).setJointVelocityRel(0.5).setMode(ctrlMode));
 		
 		//initialiser answer avec le message
-		answer = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, 
-													   "la jambe est en place?", "oui");
+		answer = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, "La jambe est-elle en place ?", "oui");
 		
 		ThreadUtil.milliSleep(tempo);
 		// Ancrage de la jambe à l'outil
 		leg.getFrame("/PnpChild").attachTo(legLift.getFrame("/dummy/pnpParent"));
 		
-		while(answer != 1){
+		do {
 			for (i=1;i<nbcycles;i++){
 				leg.getFrame("TCPKnee").move(linRel(0, 0, 0, Math.toRadians(-angle),0, 0).setCartVelocity(anglespeed));
 				leg.getFrame("TCPKnee").move(linRel(0, 0, 0, Math.toRadians(angle),0, 0).setCartVelocity(anglespeed));
 			}
 		answer = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, 
-				   "voulez vous refaire un cycle Mr/Mme :"+nom+"?", "oui", "non");			
-		}
-		answer = -1;
+				   "Voulez vous refaire un cycle Mr/Mme :"+nom+"?", "oui", "non");			
+		} while (answer == 0);
 		
 			answer = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, 
-					   "Avez vous enlever la jambe Mr/Mmme:"+nom+"?", "oui");
+					   "Avez vous enlever la jambe Mr/Mme:"+nom+"?", "oui");
 			ThreadUtil.milliSleep(tempo);
 			leg.detach();			
 		robot.move(ptpHome().setJointVelocityRel(0.5));
