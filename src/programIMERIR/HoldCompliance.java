5bbfb1e9da837ac8d5130e8d7e6191bb7e97ca8e
Aurélien <Aurlien@DESKTOP-62ON3LS>
2018-03-16 11:44:50 +0100

    --- automatic rebase to remote; old local SHA: 9bc89c4fc06a2c57fa362646624f100af854b5e7 ---

---
+++ src/programIMERIR/HoldCompliance.java
@@ -0,0 +1,53 @@
+package programIMERIR;
+
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.CartDOF;
+import com.kuka.roboticsAPI.motionModel.IMotion;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.JointImpedanceControlMode;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class HoldCompliance extends RoboticsAPIApplication {
+	@Inject
+	private LBR lBR_iiwa_14_R820_1;
+	
+	@Inject
+	private CartesianImpedanceControlMode mode;
+
+	@Override
+	public void initialize() {
+		// initialize your application here
+		mode = new CartesianImpedanceControlMode();
+		mode.parametrize(CartDOF.ALL).setStiffness(500);
+	}
+
+	@Override
+	public void run() {
+		// your application execution starts here
+		lBR_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Foam/P1")));
+		lBR_iiwa_14_R820_1.move(positionHold(mode, -1, TimeUnit.SECONDS));
+	}
+}
