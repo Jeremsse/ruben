e2d3f51872bacad90475037a0fe6f16f662c46f8
Aurélien <Aurlien@DESKTOP-62ON3LS>
2018-03-16 16:30:34 +0100

    --- automatic rebase to remote; old local SHA: 17b0334020fcddef7bbc829731678898b587122f ---

--- src/programIMERIR/AllSurfacesDrawing.java
+++ src/programIMERIR/AllSurfacesDrawing.java
@@ -1,17 +1,14 @@
 package programIMERIR;
 
 
-import static com.kuka.roboticsAPI.motionModel.BasicMotions.lin;
-import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
-import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptpHome;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
-import javax.inject.Named;
-
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
-import com.kuka.roboticsAPI.applicationModel.tasks.UseRoboticsAPIContext;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 import com.kuka.roboticsAPI.deviceModel.LBR;
-import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.motionModel.PositionHold;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
 
 /**
  * Implementation of a robot application.
@@ -31,34 +28,22 @@ import com.kuka.roboticsAPI.geometricModel.Tool;
  * @see #run()
  * @see #dispose()
  */
-public class RobotApplicationIMERIRDebut extends RoboticsAPIApplication {
+public class AllSurfacesDrawing extends RoboticsAPIApplication {
 	@Inject
 	private LBR robot;
-	@Inject
-	@Named("LegLift")
-	private Tool legLift;//Création d'un objet outil
-	private int i;
+	
+	private CartesianImpedanceControlMode freeMoveMode;
 
 	@Override
 	public void initialize() {
 		// initialize your application here
-		legLift.attachTo(robot.getFlange());//"Fixation" de l'outil à la bride du robot.
+		freeMoveMode = new CartesianImpedanceControlMode();
 	}
 
-	
 	@Override
 	public void run() {
 		// your application execution starts here
-		robot.move(ptpHome());
-		for (i=1;i < 7;i++){
-			if (i==1){
-				legLift.getFrame("TCP").move(ptp(getApplicationData().getFrame("/Foam/P"+i)));
-			}else{
-				legLift.getFrame("TCP").move(lin(getApplicationData().getFrame("/Foam/P"+i)));
-			}
-			
-		}
-		legLift.getFrame("TCP").move(lin(getApplicationData().getFrame("/Foam/P1")));
-		robot.move(ptpHome());
+		robot.move(ptp(getApplicationData().getFrame("/WorkingTable/WaitingPoint")));
+		robot.move(positionHold(freeMoveMode, -1, TimeUnit.SECONDS));
 	}
 }
