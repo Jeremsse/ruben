25482064fa46849ed86b946329f7a0cfb461958d
Alexandre CLANCIER <alexandre.clancier@imerir.com>
2018-03-19 09:22:28 +0100

    modif outils

--- src/application/GrabToolsAndWork.java
+++ src/application/GrabToolsAndWork.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.lin;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptpHome;
 
@@ -42,7 +43,9 @@ public class GrabToolsAndWork extends RoboticsAPIApplication {
 	@Named("Pliers")
 	private Tool pliers;//Création d'un objet outil de type pince
 	
-	double x, y, z;
+	double x, y, z, x2, y2, z2, x3, y3, z3, x4, y4, z4;
+	
+	double largeurOutil = 60;
 	
 	@Override
 	public void initialize() 
@@ -58,13 +61,27 @@ public class GrabToolsAndWork extends RoboticsAPIApplication {
 	x = getFrame("/Workspace/P1").getX();
 	y = getFrame("/Workspace/P1").getY();
 	z = getFrame("/Workspace/P1").getZ();
+	x2 = getFrame("/Workspace/P2").getX();
+	y2 = getFrame("/Workspace/P2").getY();
+	z2 = getFrame("/Workspace/P2").getZ();
+	x3 = getFrame("/Workspace/P3").getX();
+	y3 = getFrame("/Workspace/P3").getY();
+	z3 = getFrame("/Workspace/P3").getZ();
+	x4 = getFrame("/Workspace/P4").getX();
+	y4 = getFrame("/Workspace/P4").getY();
+	z4 = getFrame("/Workspace/P4").getZ();
 	
-	pliers.getFrame("ClampingArea").move(ptp(getApplicationData().getFrame("/Workshop/Brush")).setJointVelocityRel(1.0));
-	ThreadUtil.milliSleep(500);
+	/*double largeur;
+	largeur = x4-x;
+	double pourcentage;
+	pourcentage = largeurOutil / largeur;*/
+	
+	pliers.getFrame("ClampingArea").move(ptp(getApplicationData().getFrame("/Workspace/P1")).setJointVelocityRel(1.0));
+	for(x = getFrame("/Workspace/P1").getX(); x<x4; x+=largeurOutil)
+	{
+		pliers.getFrame("ClampingArea").move(lin().setJointVelocityRel(1.0));
+	}
 	robot.move(ptpHome());
 	
-	
-	
-	//programme détection 4 extrémités de la planche
-}
+	}
 }
