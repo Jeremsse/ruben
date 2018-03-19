dee650cf9f93a1b87cb4b207542ff0eee82ce3f9
Clement Bourdarie <clement.bourdarie@imerir.com>
2018-03-19 11:56:10 +0100

    --- automatic rebase to remote; old local SHA: 34388d160120f3629914acb38ae2c0a606dd9df9 ---

---
+++ src/application/HoldAndDo.java
@@ -0,0 +1,206 @@
+package application;
+
+
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.positionHold;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.ITransformationProvider;
+import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
+import com.kuka.roboticsAPI.geometricModel.SceneGraphObject;
+import com.kuka.roboticsAPI.geometricModel.StaticTransformationProvider;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.math.ITransformation;
+import com.kuka.roboticsAPI.geometricModel.math.XyzAbcTransformation;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.JointImpedanceControlMode;
+import com.kuka.roboticsAPI.uiModel.userKeys.IUserKey;
+import com.kuka.roboticsAPI.uiModel.userKeys.IUserKeyBar;
+import com.kuka.roboticsAPI.uiModel.userKeys.IUserKeyListener;
+import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyAlignment;
+import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyEvent;
+import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyLED;
+import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyLEDSize;
+
+/**
+ * 
+ * @author Clément Bourdarie
+ */
+public class HoldAndDo extends RoboticsAPIApplication {
+	@Inject
+	private LBR robot;
+	@Inject
+	@Named("Pliers")
+	private Tool pliers;
+
+	private JointImpedanceControlMode mode;
+	private double[] jointPosition;
+	
+	private IUserKeyBar buttonBar;
+	private IUserKey allowMovementKey;
+	private IUserKey polishKey;
+	private IUserKey registerPositionKey;
+	private IUserKey stopApplicationKey;
+
+	private boolean moving = false;
+	private boolean finished = false;
+	
+	private int currentPointIndex;//the index of the point being registered
+	
+	@Override
+	public void initialize() {
+		mode = new JointImpedanceControlMode(10, 10, 10, 10, 10, 10, 1);
+		mode.setStiffness(10, 10, 10, 10, 10, 10, 1);
+		
+		//The listener of the allowMovementKey button
+		IUserKeyListener moveButtonListener = new IUserKeyListener() {
+			@Override
+			public void onKeyEvent(IUserKey key, UserKeyEvent event) {
+				if(event.equals(UserKeyEvent.KeyUp)){
+					moving = !moving;// Allow the robot to move, or stop it.
+					key.setText(UserKeyAlignment.MiddleLeft, moving ? "ON" : "OFF");// If the robot was moving show OFF, else show ON.
+				}
+			}
+		};
+		
+		//The listener of the polishKey button
+		IUserKeyListener polishButtonListener = new IUserKeyListener() {
+			@Override
+			public void onKeyEvent(IUserKey key, UserKeyEvent event) {
+				if(event.equals(UserKeyEvent.KeyUp)){
+					polishKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Green, UserKeyLEDSize.Small);
+
+					//TODO make the wood plank polishing function (alexandre/louis)
+					polish();
+
+					polishKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+				}
+			}
+		};
+		
+		//The listener of the polishKey button
+		IUserKeyListener registerButtonListener = new IUserKeyListener() {
+			@Override
+			public void onKeyEvent(IUserKey key, UserKeyEvent event) {
+				if(event.equals(UserKeyEvent.KeyUp)){
+					registerPositionKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+
+					//TODO make the position registering function (clement)
+					registerPosition();
+
+					registerPositionKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+				}
+			}
+		};
+		
+		//The listener of the stopApplicationKey button
+		IUserKeyListener stopApplicationButtonListener = new IUserKeyListener() {
+			@Override
+			public void onKeyEvent(IUserKey key, UserKeyEvent event) {
+				if(event.equals(UserKeyEvent.KeyUp)){
+					getLogger().info("Programme terminé.");
+					finished = true;
+				}
+			}
+		};
+		
+		//The container of the button we're going to create
+		buttonBar = getApplicationUI().createUserKeyBar("Mouvement");
+		
+		//Button allowing the user to move the robot
+		allowMovementKey = buttonBar.addUserKey(0, moveButtonListener, true);
+		allowMovementKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+		
+		//Button starting the polishing
+		polishKey = buttonBar.addUserKey(1, polishButtonListener, true);
+		polishKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+
+		//Button to register a position
+		registerPositionKey = buttonBar.addUserKey(2, registerButtonListener, true);
+		registerPositionKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+		
+		//Button allowing the user to stop the application
+		stopApplicationKey = buttonBar.addUserKey(3, stopApplicationButtonListener, true);
+		stopApplicationKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+		
+		//Make the buttons bar visible
+		buttonBar.publish();
+	}
+
+	/**
+	 * Major function
+	 */
+	@Override
+	public void run() {
+		//While the user hasn't pressed the stopApplicationKey
+		while(!finished){
+			//If the user has pressed the allowMovementKey
+			if(moving){
+				allowMovement();
+			}
+		}
+	}
+	
+	/**
+	 *  Allow the user to freely move the robot and select 4 points
+	 */
+	private void allowMovement(){
+		//Make the allowMovementKey LED go RED
+		getLogger().info("The robot is compliant.");
+		allowMovementKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Green, UserKeyLEDSize.Small);
+		//Make the robot compliant according to "mode" which makes his stiffness low
+		while (moving) {
+			robot.move(positionHold(mode, 1, TimeUnit.SECONDS));
+			jointPosition = robot.getCurrentJointPosition().get();// Register the current position
+		}
+		//Make the allowMovementKey LED go RED
+		getLogger().info("Stop touching the robot.\nThe robot is not compliant anymore.");
+		allowMovementKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+		
+		//Move to the registered position so that the robot holds it
+		robot.move(ptp(jointPosition));
+	}
+	
+	/**
+	 * Make the robot polish the area between the 4 points
+	 */
+	private void polish(){
+		getLogger().info("Ponçage...");
+		
+		polishKey.setText(UserKeyAlignment.MiddleLeft, "Ponçage...");
+		polishKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Green, UserKeyLEDSize.Small);
+
+		//TODO make the polishing function
+
+		polishKey.setLED(UserKeyAlignment.MiddleLeft, UserKeyLED.Red, UserKeyLEDSize.Small);
+		
+		getLogger().info("Ponçage terminé.");
+	}
+	
+	/**
+	 * Register the current state as a position.
+	 */
+	private void registerPosition(){
+		getLogger().info("Enregistrement de la position...");
+
+		currentPointIndex = currentPointIndex == 4 ? 1 : currentPointIndex;
+		
+		//parameters
+		String pointNameString = new StringBuilder("NP").append(String.valueOf(currentPointIndex)).toString();//NP1,NP2,NP3,NP4.
+		SceneGraphObject owner = pliers;
+		ITransformation transformation = XyzAbcTransformation.ofTranslation(pliers.getFrame("/Sander").getX(), pliers.getFrame("/Sander").getY(), pliers.getFrame("/Sander").getY());
+		ITransformationProvider transformationProvider = new StaticTransformationProvider(transformation);
+		ObjectFrame parent = getApplicationData().getFrame("Workspace");
+		
+		ObjectFrame newPointFrame = new ObjectFrame(pointNameString, parent , owner, transformationProvider);
+		
+		getLogger().info(String.valueOf(transformation.getTranslation().getX()) + "|" + String.valueOf(transformation.getTranslation().getY()) + "|" + String.valueOf(transformation.getTranslation().getZ()));
+		
+		getLogger().info("Enregistrement de la position terminé");
+	}
+}
